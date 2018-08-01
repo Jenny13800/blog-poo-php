426f8e7c243a9b15e0b9064f8c5489bba968b9b7
Jennifer <jcour@montmirail.com>
2018-08-01 11:52:17 +0200

    1 etape du tp

---
+++ pages/templates/default.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title><?= \App\App::getTitle(); ?></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Navbar</a>
+        </div>
+    </div>
+</nav>
+
+<main role="main" class="container">
+
+    <div class="starter-template" style="padding-top: 100px;">
+        <?= $content; ?>
+    </div>
+
+</main><!-- /.container -->
+
+
+</body>
+</html>
